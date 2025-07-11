60925e3f0fd773b41d1936f4314187b724069b94
keshavsaini07 <sainikeshav770@gmail.com>
2025-07-11 23:09:19 +0530

    learned about number type

--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
-const x: number = 2;
-console.log(x)
+
+
+// explicit type definition
+let x: number = 2;
+
+// implicit type definition: takes automatically number as type
+let y = 2;
+
+// zero and negative zero
+let a = 0;
+let b = -0;
+
+// infinity and negative infinity
+let c = Infinity;
+let d = -Infinity;
+
+// floating point values
+let z = 30.565;
+
+// the Number starting with capital N is a wrapper around the primitive type - number.
+let h = Number; 
