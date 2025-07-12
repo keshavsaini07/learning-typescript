4f6dced29ba0fb65841f91dfbb0bfc9cba4eeef0
keshavsaini07 <sainikeshav770@gmail.com>
2025-07-12 23:58:26 +0530

    learnt about void, never, and arrays

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-
 // 1. number
 
 // explicit type definition
@@ -19,16 +18,15 @@ let d = -Infinity;
 let z = 30.565;
 
 // the Number starting with capital N is a wrapper around the primitive type - number.
-let h = Number; 
-
+let h = Number;
 
 // 2. string
 
 // explicit
-let str1 : string = "hello";   // 'hello' or `hello`
+let str1: string = "hello"; // 'hello' or `hello`
 
 // implicit
-let str2 = "123";   
+let str2 = "123";
 
 // template strings
 let val = `${str1} is ${str2}`;
@@ -51,10 +49,47 @@ let value1: null; // only null value can be assigned to value1
 
 value1 = null;
 
-console.log(value1)
+console.log(value1);
 
 // 5. undefined : If you declare a variable without assigning it a value, TypeScript automatically assigns undefined to it.
 
 let value2: undefined = undefined;
 
 console.log(value2);
+
+// 6. void : void signifies that a function completes its execution but does not return any meaningful value.  A function returning void can implicitly or explicitly return undefined or null, though typically no return statement is used.
+
+function logMessage(message: string): void {
+  console.log(message);
+}
+
+// 7. never : never signifies that a function never completes its execution normally. A function returning never cannot return any value, including undefined or null, because it never reaches a normal completion point.
+
+// 8. Arrays :
+
+// explicit
+let arr1: number[] = [1, 2, 3];
+
+// implicit
+let arr2 = [1, 2, 3];
+
+// union types - combination of types
+let arr3: (string | number)[] = [1, 2, 3, "hello", "123"];
+
+// shows error as ts does not know which data type will be fetched
+// console.log(arr3[3] + 1);
+
+// Nested Arrays
+
+var arr4: string[][] = [["hello"], ["123"], []];
+
+// type : (number | (string | number)[])[]
+var arr5: (number | (string | number)[])[] = [
+  ["hello", 1],
+  ["123", 1],
+  [1, 2, 3],
+];
+
+// Empty Array: try declaring an empty array always with its type to avoid 'any' definition
+
+var arr6: number[] = [];
